c15a7684143a5b58803565747cf58c7864b90bfe
Ryan Haney <25014745+thathaneydude@users.noreply.github.com>
2024-09-05 08:13:35 -0500

    Pinning sensor config to v0.1.0 version until v27.14.0 of the software sensor is released (#10)

--- sensor_config.tf
+++ sensor_config.tf
@@ -1,5 +1,5 @@
 module "sensor_config" {
-  source = "github.com/corelight/terraform-config-sensor"
+  source = "github.com/corelight/terraform-config-sensor?ref=v0.1.0"
 
   fleet_community_string                       = var.community_string
   sensor_license                               = var.license_key
