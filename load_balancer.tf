6be7ffd47edd4b3109a49a35f5f0592926f6b0a1
Hassan Baker <hassan.baker@corelight.com>
2025-06-12 15:47:43 +0100

    fmt

--- load_balancer.tf
+++ load_balancer.tf
@@ -40,7 +40,7 @@ resource "google_compute_region_backend_service" "traffic_ilb_backend_service" {
 
 
   backend {
-    group = google_compute_region_instance_group_manager.sensor_mig.instance_group
+    group          = google_compute_region_instance_group_manager.sensor_mig.instance_group
     balancing_mode = "CONNECTION"
   }
 }
